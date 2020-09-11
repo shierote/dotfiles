00005301f0a7b4519f0d918248b715dcf7be3947
shierote <taishi2060@gmail.com>
2020-09-11 15:43:33 +0900

    Update

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 ln -sf $PWD/.gitignore_global $HOME/.gitignore_global
 ln -sf $PWD/.vimrc $HOME/.vimrc
 ln -sf $PWD/.zshrc $HOME/.zshrc
-ln -sf $PWD/.zsh.d $HOME/.zsh.d
 ln -sf $PWD/.gitconfig $HOME/.gitconfig
+ln -nsf $PWD/.zsh.d $HOME/.zsh.d
 
 git config --global commit.template $PWD/.commit_template
 
