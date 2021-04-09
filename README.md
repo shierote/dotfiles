7251be8d08c47b86ed29830e04ba8405cd09fbc7
shierote <taishi2060@gmail.com>
2021-04-09 14:48:28 +0900

    :wrench: Add new setup

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ## install plugin
 Once you change a computer
 ```
+$ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 $ ./install.sh
 ```
 ## setup
--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
+
 # install oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.zsh/zsh-autosuggestions
+
+# install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # install pyenv
 git clone git://github.com/yyuu/pyenv.git $HOME/.pyenv
