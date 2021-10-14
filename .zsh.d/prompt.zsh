2def71345319b05990aaed5cf18b25555d9f6673
shierote <taishi2060@gmail.com>
2021-10-15 01:49:44 +0900

    Update p10k config on Linux

--- .zsh.d/prompt.zsh
+++ .zsh.d/prompt.zsh
@@ -1,18 +1,13 @@
 if [ $(uname) = "Darwin" ];then
   source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme
-
-  # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-  # Initialization code that may require console input (password prompts, [y/n]
-  # confirmations, etc.) must go above this block; everything else may go below.
-  if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-    source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-  fi
-
-  # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-  [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-else
-  ZSH_THEME="powerlevel10k/powerlevel10k"
+elif [ $(uname) = "Linux" ];then
+  source $HOME/.oh-my-zsh/custom/themes/powerlevel10k/powerlevel10k.zsh-theme
 fi
+
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
 plugins=(
   git ruby zsh-autosuggestions
