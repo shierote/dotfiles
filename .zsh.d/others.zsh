009a56b43dd5c53852d752791dddf8af4f7a74c5
shierote <taishi2060@gmail.com>
2021-11-07 15:09:41 +0900

    Enable to search history with command prefix

--- .zsh.d/others.zsh
+++ .zsh.d/others.zsh
@@ -1,1 +1,7 @@
 source "$HOME/.cargo/env"
+
+autoload -U history-search-end
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
+bindkey "^[[A" history-beginning-search-backward-end
+bindkey "^[[B" history-beginning-search-forward-end
