70adbdca0eafecb811e2c86255a0727122d54835
shierote <taishi2060@gmail.com>
2021-11-11 20:38:10 +0900

    Fix env diff

--- .zsh.d/others.zsh
+++ .zsh.d/others.zsh
@@ -3,5 +3,13 @@ source "$HOME/.cargo/env"
 autoload -U history-search-end
 zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
-bindkey "$terminfo[kcuu1]" history-beginning-search-backward-end
-bindkey "$terminfo[kcud1]" history-beginning-search-forward-end
+
+if [ $(uname) = "Darwin" ];then
+  # macOS
+  bindkey "^[[A" history-beginning-search-backward-end
+  bindkey "^[[B" history-beginning-search-forward-end
+elif [ $(uname) = "Linux" ];then
+  # Ubuntu
+  bindkey "$terminfo[kcuu1]" history-beginning-search-backward-end
+  bindkey "$terminfo[kcud1]" history-beginning-search-forward-end
+fi
